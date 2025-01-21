3d88fe762cce868066a115ea4d703b11f4e18f51
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-01-21 14:01:01 +0100

    create task.txt, main.py in home_work_1

---
+++ home_work_1/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hallo_flask():
+    return "Hello, Flask!"
+
+
+@app.route('/user/<name>')
+def hallo_name(name):
+    return f"Hello, {name}!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
