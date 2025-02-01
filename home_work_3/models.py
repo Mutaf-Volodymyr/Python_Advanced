ed91dd1d246d329b80f5a3680d5f42617e7fb5e8
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-02-01 16:43:30 +0100

    Done hw_4

--- home_work_3/models.py
+++ home_work_3/models.py
@@ -1,27 +1,37 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Numeric
-from sqlalchemy.orm import as_declarative, declared_attr, relationship
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
 
 
-@as_declarative()
-class AbstractModel:
+class Product(Base):
+    __tablename__ = 'products'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
-
-    @classmethod
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
-
-
-class Product(AbstractModel):
     name = Column(String(100), nullable=False, unique=True)
     price = Column(Numeric(10, 2), nullable=False)
     in_stock = Column(Boolean, nullable=False)
-    category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
-    category = relationship("Category", back_populates="product")
+    category_fk = Column(Integer, ForeignKey('category.id'), nullable=False)
+    category = relationship("Category", back_populates="product", uselist=False, lazy="joined")
+
+    def __repr__(self):
+        return f"<Product(name='{self.name}', price={self.price}, in_stock={self.in_stock})>"
+
+    def __str__(self):
+        return self.name
 
 
-class Category(AbstractModel):
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False, unique=True)
     description = Column(String(500))
-    product = relationship("Product", back_populates="category")
+    product = relationship("Product", back_populates="category", uselist=True, lazy="joined")
+
+    def __repr__(self):
+        return f"<Category(name='{self.name}')>"
+
+    def __str__(self):
+        return self.name
+
 
