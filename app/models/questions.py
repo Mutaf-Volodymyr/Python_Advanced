dc5465a3ad58df6fd6e68ad0027aa2cb574d813b
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-02-03 12:33:27 +0100

    create Category and migrate

--- app/models/questions.py
+++ app/models/questions.py
@@ -7,6 +7,8 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.String, nullable=False)
     responses = db.relationship('Response', backref='question', lazy='joined', uselist=True)
+    statistic = db.relationship('Statistic', backref='question', lazy='joined', uselist=True)
+    category = db.relationship('Category', back_populates='question', uselist=False, lazy='joined')
 
     def __repr__(self):
         return '<Question %r>' % self.text
@@ -20,3 +22,10 @@ class Statistic(db.Model):
     def __repr__(self):
         return '<Statistic for Question %r: %r agree, %r disagree>' % (
         self.question_id, self.agree_count, self.disagree_count)
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    question = db.relationship('Question', back_populates='category', uselist=True, lazy='joined')
